7073477ee5c8c3128bf83cab2c65f3ee4e8cc6cb
Guilherme Monni <gmonni20@gmail.com>
2025-11-25 22:07:40 -0300

    fix cors 2.0

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const server = fastify({ logger: true })
 await server.register(cors, { 
-    origin: ['https://kindlyhelp.vercel.app/', 'http://localhost:5173'],
+    origin: 'https://kindlyhelp.vercel.app/',
     credentials: true
 })
 
